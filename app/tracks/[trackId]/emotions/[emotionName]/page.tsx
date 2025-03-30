684d44f6f5143d5e1bd7933c60aaa865cec08fa2
ryhhill1998 <ryhhill1998@outlook.com>
2025-03-30 10:23:47 +0100

    improved appearance/layout of tagged lyrics page

--- app/tracks/[trackId]/emotions/[emotionName]/page.tsx
+++ app/tracks/[trackId]/emotions/[emotionName]/page.tsx
@@ -16,31 +16,45 @@ const EmotionPage = async ({
 	);
 
 	return (
-		<div className="container mx-auto flex gap-4">
-			<div className="flex flex-col gap-2">
-				<Image
-					src={track.images[0].url}
-					alt={"Album image for track"}
-					width={200}
-					height={200}
-				/>
+		<div className="container mx-auto bg-stone-900 p-4 rounded-md">
+			<header className="text-white p-4 font-bold text-lg mb-8">
+				<h1>
+					Your Top{" "}
+					{emotionName.charAt(0).toUpperCase() + emotionName.slice(1)}{" "}
+					Track
+				</h1>
+			</header>
 
-				<p className="text-white">{track.name}</p>
+			<div className="flex justify-center gap-20">
+				<div className="flex flex-col gap-2">
+					<Image
+						src={track.images[0].url}
+						alt={"Album image for track"}
+						width={250}
+						height={250}
+					/>
 
-				<p className="text-white">{track.artist.name}</p>
+					<p className="text-white text-2xl font-bold">
+						{track.name}
+					</p>
 
-				<div className="flex gap-2">
-					<p className="text-white">{track.albumName}</p>
-					<p className="text-white">{track.releaseDate}</p>
+					<p className="text-stone-300 text-lg font-bold">
+						{track.artist.name}
+					</p>
+
+					<div className="flex gap-2 text-stone-400 text-sm font-semibold">
+						<p>{track.albumName}</p>
+						<p>{track.releaseDate}</p>
+					</div>
 				</div>
-			</div>
 
-			<div
-				dangerouslySetInnerHTML={{
-					__html: taggedLyrics,
-				}}
-				className="text-gray-300"
-			></div>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: taggedLyrics,
+					}}
+					className="text-stone-300"
+				></div>
+			</div>
 		</div>
 	);
 };
