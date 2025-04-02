e48738ef185cb46097d558fb4e463acd25d969fe
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-02 17:39:10 +0100

    updated sign in button route

--- app/(routes)/login/components/SpotifySignInButton.tsx
+++ app/(routes)/login/components/SpotifySignInButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const handleSpotifyLogin = () => {
 	window.location.href = `${API_BASE_URL}/auth/spotify/login`;
