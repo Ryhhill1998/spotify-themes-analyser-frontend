3f4bc66b0624c9847d6933893333e02ed47373a2
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-02 14:58:07 +0100

    improved profile skeleton appearance

--- app/(routes)/(auth)/profile/components/profile/ProfileBannerSkeleton.tsx
+++ app/(routes)/(auth)/profile/components/profile/ProfileBannerSkeleton.tsx
@@ -3,9 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 const ProfileBannerSkeleton = () => {
 	return (
 		<div className="flex items-center gap-4 p-6 bg-stone-700 rounded-t-md">
-			<Skeleton className="w-[200px] h-[200px] aspect-square object-cover rounded-full" />
+			<Skeleton className="w-[225px] h-[225px] aspect-square object-cover rounded-full" />
 
-			<div className="flex flex-col gap-2 w-full">
+			<div className="flex flex-col gap-4 w-full">
+				<Skeleton className="h-3 w-full max-w-[5%]" />
 				<Skeleton className="h-15 w-full max-w-[25%]" />
 				<Skeleton className="h-3 w-full max-w-[35%]" />
 				<Skeleton className="h-3 w-full max-w-[10%]" />
