6a44e5e46442e14f76a3d4b45ace7045111184ad
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-03 09:55:52 +0100

    test

--- app/(routes)/login/components/SpotifySignInButton.tsx
+++ app/(routes)/login/components/SpotifySignInButton.tsx
@@ -3,7 +3,8 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const handleSpotifyLogin = () => {
-	window.location.href = `${API_BASE_URL}/auth/spotify/login`;
+	// window.location.href = `${API_BASE_URL}/auth/spotify/login`;
+	window.location.href = `${API_BASE_URL}/docs`;
 };
 
 const SpotifySignInButton = () => {
