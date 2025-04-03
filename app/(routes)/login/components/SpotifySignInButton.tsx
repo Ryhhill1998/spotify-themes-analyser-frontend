589d2a6343bb8a7efca2c949cee6ce8efdb78796
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-03 11:04:30 +0100

    cookie setting test

--- app/(routes)/login/components/SpotifySignInButton.tsx
+++ app/(routes)/login/components/SpotifySignInButton.tsx
@@ -2,8 +2,8 @@
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const handleSpotifyLogin = () => {
-	window.location.href = `${API_BASE_URL}/auth/spotify/login`;
+const handleSpotifyLogin = async () => {
+	window.location.href = `${API_BASE_URL}/auth/spotify/cookies`;
 };
 
 const SpotifySignInButton = () => {
