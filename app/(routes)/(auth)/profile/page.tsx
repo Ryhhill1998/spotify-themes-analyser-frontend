7c19bffca0a42cc7a9b230d34dfe7d5c1b5c27a5
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-04 10:33:56 +0100

    test

--- app/(routes)/(auth)/profile/page.tsx
+++ app/(routes)/(auth)/profile/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react";
 
 import ProfileBanner from "@/app/(routes)/(auth)/profile/components/profile/ProfileBanner";
-import TopArtistsSection from "@/app/(routes)/(auth)/profile/components/artists/TopArtistsSection";
-import TopTracksSection from "@/app/(routes)/(auth)/profile/components/tracks/TopTracksSection";
+// import TopArtistsSection from "@/app/(routes)/(auth)/profile/components/artists/TopArtistsSection";
+// import TopTracksSection from "@/app/(routes)/(auth)/profile/components/tracks/TopTracksSection";
 import ProfileBannerSkeleton from "@/app/(routes)/(auth)/profile/components/profile/ProfileBannerSkeleton";
-import TopTracksSectionSkeleton from "@/app/(routes)/(auth)/profile/components/tracks/TopTracksSectionSkeleton";
-import TopArtistsSectionSkeleton from "@/app/(routes)/(auth)/profile/components/artists/TopArtistsSectionSkeleton";
+// import TopTracksSectionSkeleton from "@/app/(routes)/(auth)/profile/components/tracks/TopTracksSectionSkeleton";
+// import TopArtistsSectionSkeleton from "@/app/(routes)/(auth)/profile/components/artists/TopArtistsSectionSkeleton";
 
 const ProfilePage = () => {
 	return (
