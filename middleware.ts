276f96cdf15371a11f7ecf2dc57ce81762a8ea0c
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-04 07:36:38 +0100

    added refresh token functionality to middleware

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,11 @@ const publicRoutes = ["/", "/login"];
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+type Tokens = {
+	access_token: string;
+	refresh_token: string;
+};
+
 const middleware = async (req: NextRequest) => {
 	const path = req.nextUrl.pathname;
 
@@ -21,30 +26,45 @@ const middleware = async (req: NextRequest) => {
 	const isPublicRoute = publicRoutes.includes(path);
 
 	const cookieStore = await cookies();
-
-	const isAuthenticated =
-		cookieStore.has("access_token") && cookieStore.has("refresh_token");
-
-	cookieStore.set({ name: "test", value: "test" });
+	const accessToken = cookieStore.get("access_token");
+	const refreshToken = cookieStore.get("refresh_token");
 
 	// Redirect to /login if the user is not authenticated
-	if ((isProtectedRoute || path === "/") && !isAuthenticated) {
+	if ((isProtectedRoute || path === "/") && !(accessToken && refreshToken)) {
 		return NextResponse.redirect(new URL("/login", req.nextUrl));
 	}
 
 	// Redirect to /profile if the user is authenticated
-	if (isPublicRoute && isAuthenticated) {
+	if (isPublicRoute && accessToken && refreshToken) {
 		return NextResponse.redirect(new URL("/profile", req.nextUrl));
 	}
 
-	const res = await fetch(`${API_BASE_URL}/auth/spotify/test-refresh`, {
-		method: "POST",
-	});
-	const refreshData = await res.json();
-	cookieStore.set({ name: "access_", value: refreshData["access_"] });
-	cookieStore.set({ name: "refresh_", value: refreshData["refresh_"] });
-
+	// create response object
 	const response = NextResponse.next();
+
+	// Refresh tokens if accessToken has expired but refreshToken exists
+	if (!accessToken && refreshToken) {
+		try {
+			const res = await fetch(
+				`${API_BASE_URL}/auth/spotify/refresh-tokens`,
+				{
+					method: "POST",
+				}
+			);
+			const refreshData: Tokens = await res.json();
+			response.cookies.set({
+				name: "access_token",
+				value: refreshData.access_token,
+			});
+			response.cookies.set({
+				name: "refresh_token",
+				value: refreshData.refresh_token,
+			});
+		} catch (error) {
+			cookieStore.delete("access_token");
+			return NextResponse.redirect(new URL("/login", req.nextUrl));
+		}
+	}
 
 	return response;
 };
