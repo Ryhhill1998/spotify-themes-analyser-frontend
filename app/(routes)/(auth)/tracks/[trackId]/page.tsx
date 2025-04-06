3fc875ec0c8ec1dad0b09e2a0faa50293f845c9c
ryhhill1998 <ryhhill1998@outlook.com>
2025-04-06 14:04:01 +0100

    updated emotional tags page skeleton

--- app/(routes)/(auth)/tracks/[trackId]/page.tsx
+++ app/(routes)/(auth)/tracks/[trackId]/page.tsx
@@ -12,8 +12,6 @@ const TrackPage = async ({
 	return (
 		<div className="container mx-auto bg-stone-900 rounded-md">
 			<div className="p-6">
-				<TrackDetailsSkeleton />
-
 				<Suspense fallback={<TrackDetailsSkeleton />}>
 					<TrackDetails trackId={trackId} />
 				</Suspense>
