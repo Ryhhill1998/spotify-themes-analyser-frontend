94a42a19037e40284eefe24835db74eb1838f0d0
ryhhill1998 <ryhhill1998@outlook.com>
2025-07-27 15:12:03 +0100

    fixed issue with width of bars

--- app/(routes)/(auth)/components/common/TopItemsBarChart.tsx
+++ app/(routes)/(auth)/components/common/TopItemsBarChart.tsx
@@ -9,24 +9,29 @@ const TopItemsBarChart = async ({ topItems }: TopItemsBarChartProps) => {
 	const max = Math.max(...topItems.map((d) => d.percentage));
 
 	return (
-		<div className="bg-black text-white text-sm p-6 rounded-xl space-y-8 w-full">
+		<div className="bg-black text-white text-xs sm:text-sm sm:p-6 px-4 py-6 rounded-xl space-y-8 w-full">
 			{topItems.map(({ name, percentage, positionChange }, index) => (
 				<div
 					key={name}
-					className="grid grid-cols-[150px_1fr] items-center gap-4"
+					className="grid grid-cols-[auto_minmax(0,_25%)_1fr] items-center" // Changed 10px to auto for PositionIndicator
 				>
-					<div className="flex items-center justify-start">
-						<PositionIndicator
-							position={index + 1}
-							positionChange={positionChange}
-						/>
+					{/* PositionIndicator: Ensure it has appropriate width or styling if needed */}
+					<PositionIndicator
+						position={index + 1}
+						positionChange={positionChange}
+					/>
 
-						<p>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
-					</div>
-					{/* <div className="truncate">{item.genre}</div> */}
-					<div className="bg-white/10 rounded h-8 w-full">
+					{/* Name: Apply truncation classes and optional margin */}
+					<p className="truncate pr-2">
+						{" "}
+						{/* Added ml-2 for spacing and 'truncate' class */}
+						{name.charAt(0).toUpperCase() + name.slice(1)}
+					</p>
+
+					{/* Bar */}
+					<div className="bg-white/10 rounded sm:h-8 h-6 w-full">
 						<div
-							className="bg-white h-8 rounded"
+							className="bg-white sm:h-8 h-6 rounded"
 							style={{
 								width: `${(percentage / max) * 100}%`,
 							}}
